5c95e9cb9131a8f81a05862d46ad2b68ae90e715
kenjiuno <ku@digitaldolphins.jp>
2021-06-24 01:35:11 +0900

    Adding some tests.

---
+++ src/itextsharp.test/PRTokeniserTest.cs
@@ -0,0 +1,34 @@
+﻿using iTextSharp.text.pdf;
+using NUnit.Framework;
+using System;
+using System.Text;
+
+namespace itextsharp.test
+{
+    public class PRTokeniserTest
+    {
+        [Test]
+        public void Test1InputToken()
+        {
+            var tokeniser = new PRTokeniser(Encoding.ASCII.GetBytes("123.456"));
+            tokeniser.NextValidToken();
+            Assert.AreEqual("123.456", tokeniser.StringValue);
+        }
+
+        [Test]
+        public void Test2InputTokens()
+        {
+            var tokeniser = new PRTokeniser(Encoding.ASCII.GetBytes("123.456 567.890"));
+            tokeniser.NextValidToken();
+            Assert.AreEqual("123.456", tokeniser.StringValue);
+        }
+
+        [Test]
+        public void Test3InputTokens()
+        {
+            var tokeniser = new PRTokeniser(Encoding.ASCII.GetBytes("123.456 567.890 ABC"));
+            tokeniser.NextValidToken();
+            Assert.AreEqual("123.456", tokeniser.StringValue);
+        }
+    }
+}
