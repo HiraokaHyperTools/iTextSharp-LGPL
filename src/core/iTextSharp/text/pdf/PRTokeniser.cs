05a8a5e12087ef1548cc8eacc917eab2c494755c
kenjiuno <ku@digitaldolphins.jp>
2021-06-24 00:38:22 +0900

    0.1.14 - Fix PRTokeniser overrides first value with second value when we read `781.688 1052.63`.

--- src/core/iTextSharp/text/pdf/PRTokeniser.cs
+++ src/core/iTextSharp/text/pdf/PRTokeniser.cs
@@ -266,8 +266,14 @@ namespace iTextSharp.text.pdf {
             // if we hit here, the file is either corrupt (stream ended unexpectedly),
             // or the last token ended exactly at the end of a stream.  This last
             // case can occur inside an Object Stream.
+            if (type != TK_OTHER || !stringValue.Equals("R"))
+            {
+                file.Seek(ptr);
+                type = TK_NUMBER;
+                stringValue = n1;
+            }
         }
-    
+
         public bool NextToken() {
             int ch = 0;
             do {
