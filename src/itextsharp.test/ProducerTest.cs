bd4b735676c42f4d93ad25e975ef516e4ee3352f
kenjiuno <ku@digitaldolphins.jp>
2022-02-02 15:38:43 +0900

    `/Producer` 破損チェックを追加

---
+++ src/itextsharp.test/ProducerTest.cs
@@ -0,0 +1,40 @@
+﻿using iTextSharp.text.pdf;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace itextsharp.test
+{
+    public class ProducerTest
+    {
+        [Test]
+        public void ProducerModification()
+        {
+            var newPdf = TestFileUtil.NewTempPdfFilePath();
+
+            using (var outSt = File.Create(newPdf))
+            {
+                using (var inSt = File.OpenRead(TestFileUtil.Locate(@"Word.pdf")))
+                {
+                    var reader = new PdfReader(inSt);
+                    var stamper = new PdfStamper(reader, outSt);
+
+                    stamper.Close();
+                }
+            }
+
+            using (var inSt = File.OpenRead(newPdf))
+            {
+                var reader = new PdfReader(inSt);
+
+                StringAssert.StartsWith(
+                    "Microsoft® Word 2013; modified using iTextSharp",
+                    (string)reader.Info["Producer"]
+                );
+            }
+        }
+    }
+}
---
+++ src/itextsharp.test/TestFileUtil.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace itextsharp.test
+{
+    internal static class TestFileUtil
+    {
+        internal static string Locate(string path)
+        {
+            return Path.Combine(
+                TestContext.CurrentContext.WorkDirectory,
+                "Files",
+                path
+            );
+        }
+
+        internal static string NewTempPdfFilePath()
+        {
+            return Path.Combine(
+                TestContext.CurrentContext.WorkDirectory,
+                Guid.NewGuid().ToString("N") + ".pdf"
+            );
+        }
+    }
+}
